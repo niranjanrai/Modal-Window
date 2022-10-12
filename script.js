e4dfacb962e105cbcc1b3d3a4f6b46ee8890450d
niranjanrai <rainiranjan1998@gmail.com>
2022-10-12 14:18:15 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log(btnsOpenModal[i].textContent);
+  });
+}
+console.log(btnsOpenModal);
